c839826fcc60939f2f699dc1a80df8f8c895c7be
Darryl Charles <dk.charles@ulster.ac.uk>
2024-05-28 15:28:55 +0000

    On Codespaces

--- obj/Debug/net6.0/game.AssemblyInfo.cs
+++ obj/Debug/net6.0/game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62aad05ef580ea8dfa0a97fe9034e8d95cdacc14")]
 [assembly: System.Reflection.AssemblyProductAttribute("game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
